02fd26cfb51164c281c3ebb4d57fff36e6756f61
rs545837 <114828377+rs545837@users.noreply.github.com>
2025-01-01 02:43:17 -0800

    Update train_enwik8_char.py

--- config/train_enwik8_char.py
+++ config/train_enwik8_char.py
@@ -9,7 +9,7 @@ log_interval = 10 # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
+wandb_log = True # override via command line if you like
 wandb_project = 'enwik8-char'
 wandb_run_name = 'mini-gpt'
 
@@ -25,8 +25,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 100000
+lr_decay_iters = 100000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
